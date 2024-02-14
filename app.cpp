83bdcce33e2312cb78611db1c90ffb64b3b4477c
abdobassel <abdobassel@gmail.com>
2024-02-14 11:02:55 +0200

    to lower to upper  test  and aski

--- app.cpp
+++ app.cpp
@@ -5,16 +5,20 @@ int main()
 {
     string name = "ElZeRo";
 
+    cout << tolower(name[0]) << "\n"; // 101 e
+    cout << toupper(name[0]) << "\n"; // 69 E
+    // without char == result will be aski ==> number
+
     for (int ch = 0; ch < 6; ch++)
     {
         if (char(isupper(name[ch])))
         {
 
-            cout << char(tolower(name[ch])) << "\n";
+            cout << char(tolower(name[ch]));
         }
         else
         {
-            cout << char(toupper(name[ch])) << "\n";
+            cout << char(toupper(name[ch]));
         }
     }
 
