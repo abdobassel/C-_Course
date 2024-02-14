63179d570915fde6b928e7323c4c5e90521c016c
abdobassel <abdobassel@gmail.com>
2024-02-14 12:44:24 +0200

    recurtion func

--- app.cpp
+++ app.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <cctype>
 using namespace std;
+
+int add(int num)
+{
+    if (num == 0)
+    {
+        return 0;
+    }
+    return num + add(num - 1);
+}
 int main()
 {
     string name = "ElZeRo";
+    string nametwospace = "El Ze  R  o";
 
     cout << tolower(name[0]) << "\n"; // 101 e
     cout << toupper(name[0]) << "\n"; // 69 E
@@ -21,7 +31,9 @@ int main()
             cout << char(toupper(name[ch]));
         }
     }
-
+    cout << "\n";
+    cout << add(5);
     return 0;
 }
+
 // hello bassel
