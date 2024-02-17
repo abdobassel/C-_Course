ebe805b12c1f12f7b4e6d6be1a062a4c4ee88925
abdobassel <abdobassel@gmail.com>
2024-02-17 19:07:27 +0200

    problem 1 and 2 playlist abohdhood

--- app.cpp
+++ app.cpp
@@ -33,6 +33,27 @@ int main()
     }
     cout << "\n";
     cout << add(5);
+    int age;
+    bool hasldriver;
+    bool haswasta;
+
+    cout << "your age \n";
+
+    cin >> age;
+    cout << "li driver? 1 == yes || 0 == no \n";
+    cin >> hasldriver;
+    cout << "wasta ? 1 == yes || 0 == no \n";
+    cin >> haswasta;
+
+    if (age >= 21 && hasldriver == true || haswasta == true)
+    {
+        cout << "hired";
+    }
+    else
+    {
+        cout << "regicted";
+    }
+
     return 0;
 }
 
